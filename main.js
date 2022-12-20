e9a3341e81ce1369ea73de7f9526c86383381226
yuna <yureve5772@gmail.com>
2022-12-20 21:15:48 +0900

    Add: 할일 추가하기

--- main.js
+++ main.js
@@ -5,3 +5,35 @@
 // 끝난 할일은 되돌리기 버튼을 누르면 다시 되돌릴 수 있다.
 // 탭을 이용해 아이템들을 상태별로 나누어서 볼 수 있다.
 // 모바일 버전에서도 확인 할 수 있는 반응형 웹이다.
+
+// 객체를 생각하면서 todolist를 만들어보자
+
+let inputTask = document.getElementById("input-area");
+let addButton = document.getElementById("add-button");
+let taskList = [];
+addButton.addEventListener("click", onAdd);
+
+function onAdd() {
+  let task = {
+    taskContent: inputTask.value,
+    isComplete: false,
+  };
+  taskList.push(task);
+  console.log(taskList);
+  render();
+}
+
+function render() {
+  let resultHTML = "";
+
+  for (let i = 0; i < taskList.length; i++) {
+    resultHTML += `<div class="task-area">
+    <div>집가기</div>
+    <div>
+      <button>check</button>
+      <button>Delete</button>
+    </div>
+  </div>`;
+  }
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
